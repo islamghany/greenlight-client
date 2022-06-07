05f9b6116d0fb843d530669fb6ab982aa0e66c7f
islamghany <islamghany3@gmail.com>
2022-06-08 00:54:53 +0200

    edited build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -8,47 +8,50 @@ module.exports = merge(baseConfig, {
   output: {
     publicPath: '/dist/',
   },
-
   optimization: {
-    minimizer: [
-      new TerserWebpackPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-          },
-          sourceMap: true,
-        },
-        parallel: true,
-      }),
-    ],
-    chunkIds: 'named',
-    moduleIds: 'deterministic',
-    splitChunks: {
-      name: false,
-      chunks: 'all',
-      minChunks: 1,
-      // maxAsyncRequests: 5,
-      // maxInitialRequests: 5,
-      cacheGroups: {
-        vendor: {
-          test: /node_modules/,
-          name: 'vendor',
-          chunks: 'all',
-          enforce: true,
-        },
-        react: {
-          test: /react|react-dom|mobx|prop-type/,
-          name: 'react',
-          priority: 10,
-          chunks: 'initial',
-          enforce: true,
-        },
-      },
-    },
-    runtimeChunk: {
-      name: (entrypoint) => `runtime-${entrypoint.name}`,
-    },
+    minimize: true,
+    minimizer: [new TerserWebpackPlugin()],
   },
+  // optimization: {
+  //   minimizer: [
+  //     new TerserWebpackPlugin({
+  //       terserOptions: {
+  //         compress: {
+  //           drop_console: true,
+  //         },
+  //         sourceMap: true,
+  //       },
+  //       parallel: true,
+  //     }),
+  //   ],
+  // chunkIds: 'named',
+  // moduleIds: 'deterministic',
+  // splitChunks: {
+  //   name: false,
+  //   chunks: 'all',
+  //   minChunks: 1,
+  //   // maxAsyncRequests: 5,
+  //   // maxInitialRequests: 5,
+  //   cacheGroups: {
+  //     vendor: {
+  //       test: /node_modules/,
+  //       name: 'vendor',
+  //       chunks: 'all',
+  //       enforce: true,
+  //     },
+  //     react: {
+  //       test: /react|react-dom|mobx|prop-type/,
+  //       name: 'react',
+  //       priority: 10,
+  //       chunks: 'initial',
+  //       enforce: true,
+  //     },
+  //   },
+  // },
+  // runtimeChunk: {
+  //   name: (entrypoint) => `runtime-${entrypoint.name}`,
+  // },
+  // },
   // plugins: [
   //   new BundleAnalyzerPlugin({
   //     analyzerMode: 'static',
